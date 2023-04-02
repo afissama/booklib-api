c98f161e5e2c69eed13a7ce13d22f1d4041149d6
Afis KOUSSE <mahoutinafis@gmail.com>
2023-04-02 10:32:08 +0100

    swagger ui

--- src/main/java/dev/bluesama/books/BooksApplication.java
+++ src/main/java/dev/bluesama/books/BooksApplication.java
@@ -3,10 +3,12 @@ package dev.bluesama.books;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BooksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApplication.class, args);
 	}
+
 }
